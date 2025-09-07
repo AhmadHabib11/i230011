377af4d50a4114fb689097c19a127abb99eb4334
Ahmad Habib <ahmadd.habibb11@gmail.com>
2025-09-07 21:22:08 +0500

    feedpage

--- app/src/main/java/com/example/assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/assignment_1/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginpage)) { v, insets ->
+        setContentView(R.layout.feedpage)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.feedpage1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
